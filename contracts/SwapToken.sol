2c9b2f2f1e9bf799fd43a7622a1b2548d705f898
khushprajapati1024 <khushprajapati777@gmail.com>
2024-07-20 16:43:37 +0530

    1:57:00

---
+++ contracts/SwapToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-Later
+pragma solidity >=0.7.0 < 0.9.0;
+pragma abicoder v2;
+
+import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
+import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
+
+
+contract SingleSwapToken {
+    ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+
+    address public constant DAI = ;
+    address public constant WETH9 = ;
+    address public constant USDC = ; 
+}
