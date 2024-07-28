9bbdb182fde4344af06ea816b7695cf3dd5d04c6
Archan Pandya <pandyaarchan17504@gmail.com>
2024-07-28 20:56:56 +0530

    3:01:28

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -1,66 +1,88 @@
-// SPDX-License-Identifier: <SPDX-License>
-pragma solidity ^0.7.6; 
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.7.6;
 pragma abicoder v2;
 
 import "@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol";
 import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 contract SingleSwapToken {
-    ISwapRouter public constant swapRouter = ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
+    ISwapRouter public constant swapRouter =
+        ISwapRouter(0xE592427A0AEce92De3Edee1F18E0157C05861564);
 
     address public constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     address public constant WETH9 = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; 
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
-    function swapExactInputSingle(uint amountIn) external returns(uint amountOut) {
+    function swapExactInputSingle(
+        uint amountIn
+    ) external returns (uint amountOut) {
         // Transfer the specified amount of WETH9 to this contract.
-        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountIn);
+        TransferHelper.safeTransferFrom(
+            WETH9,
+            msg.sender,
+            address(this),
+            amountIn
+        );
 
         // Approve the router to spend WETH9.
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountIn);
 
         // Set up the parameters for the swap.
-        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter.ExactInputSingleParams({
-            tokenIn: WETH9,
-            tokenOut: DAI,
-            fee: 3000,
-            recipient: msg.sender,
-            deadline: block.timestamp,
-            amountIn: amountIn,
-            amountOutMinimum: 0,
-            sqrtPriceLimitX96: 0
-        });
+        ISwapRouter.ExactInputSingleParams memory params = ISwapRouter
+            .ExactInputSingleParams({
+                tokenIn: WETH9,
+                tokenOut: DAI,
+                fee: 3000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountIn: amountIn,
+                amountOutMinimum: 0,
+                sqrtPriceLimitX96: 0
+            });
 
         // Perform the swap.
         amountOut = swapRouter.exactInputSingle(params);
     }
 
-    function swapExactOutputSingle(uint amountOut, uint amountInMaximum) external returns(uint amountIn) {
+    function swapExactOutputSingle(
+        uint amountOut,
+        uint amountInMaximum
+    ) external returns (uint amountIn) {
         // Transfer the specified amount of WETH9 to this contract.
-        TransferHelper.safeTransferFrom(WETH9, msg.sender, address(this), amountInMaximum);
+        TransferHelper.safeTransferFrom(
+            WETH9,
+            msg.sender,
+            address(this),
+            amountInMaximum
+        );
 
         // Approve the router to spend WETH9.
         TransferHelper.safeApprove(WETH9, address(swapRouter), amountInMaximum);
 
         // Set up the parameters for the swap.
-        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter.ExactOutputSingleParams({
-            tokenIn: WETH9,
-            tokenOut: DAI,
-            fee: 3000,
-            recipient: msg.sender,
-            deadline: block.timestamp,
-            amountOut: amountOut,
-            amountInMaximum: amountInMaximum,
-            sqrtPriceLimitX96: 0
-        });
+        ISwapRouter.ExactOutputSingleParams memory params = ISwapRouter
+            .ExactOutputSingleParams({
+                tokenIn: WETH9,
+                tokenOut: DAI,
+                fee: 3000,
+                recipient: msg.sender,
+                deadline: block.timestamp,
+                amountOut: amountOut,
+                amountInMaximum: amountInMaximum,
+                sqrtPriceLimitX96: 0
+            });
 
         // Perform the swap.
         amountIn = swapRouter.exactOutputSingle(params);
 
         // If the actual amount spent is less than the allowed maximum, refund the difference.
-        if(amountIn < amountInMaximum) {
+        if (amountIn < amountInMaximum) {
             TransferHelper.safeApprove(WETH9, address(swapRouter), 0);
-            TransferHelper.safeTransfer(WETH9, msg.sender, amountInMaximum - amountIn);
+            TransferHelper.safeTransfer(
+                WETH9,
+                msg.sender,
+                amountInMaximum - amountIn
+            );
         }
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,15 +1,34 @@
 const hre = require("hardhat");
 
 async function main() {
-    const Lock = await hre.ethers.getContractFactory("Lock");
-    const lock = await Lock.deploy();
+  //ERC20 Boo Token
+  const BooToken = await hre.ethers.getContractFactory("BooToken");
+  const booToken = await BooToken.deploy();
+  await booToken.deployed();
+  console.log(`BOO deployed to ${booToken.address}`);
 
-    await lock.deployed();
+  //ERC20 LIFE Token
+  const LifeToken = await hre.ethers.getContractFactory("LifeToken");
+  const lifeToken = await LifeToken.deploy();
+  await lifeToken.deployed();
+  console.log(`LIFE deployed to ${lifeToken.address}`);
 
-    console.log(`deployed to ${lock.address}`);
+  //SingleSwap Token
+  const SingleSwapToken = await hre.ethers.getContractFactory(
+    "SingleSwapToken"
+  );
+  const singleSwapToken = await SingleSwapToken.deploy();
+  await singleSwapToken.deployed();
+  console.log(`SINGLESWAP deployed to ${singleSwapToken.address}`);
+
+  //MultiHop Token
+  const SwapMultiHop = await hre.ethers.getContractFactory("SwapMultiHop");
+  const swapMultiHop = await SwapMultiHop.deploy();
+  await swapMultiHop.deployed();
+  console.log(`MULTIHOP deployed to ${swapMultiHop.address}`);
 }
 
 main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
+  console.error(error);
+  process.exitCode = 1;
 });
